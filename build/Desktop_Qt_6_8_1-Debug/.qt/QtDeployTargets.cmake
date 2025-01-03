be802319a17c0520839327e13ea2f28a2e42b88d
Wander S Moschetta <wander.sm@gmail.com>
2025-01-03 19:22:43 -0300

    start project

---
+++ build/Desktop_Qt_6_8_1-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appheva-system_FILE /home/wander/Projetos/kawsoft/robotic/heva-system/src/heva-system/build/Desktop_Qt_6_8_1-Debug/appheva-system)
+set(__QT_DEPLOY_TARGET_appheva-system_TYPE EXECUTABLE)
